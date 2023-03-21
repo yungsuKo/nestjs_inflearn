fdccc8d093c1ee190d100ab5c9c65e549b322a20
Tom Ko <yungsu2391@gmail.com>
2023-03-21 09:27:50 +0900

    mysql db 연동, graphql api

---
+++ class/13-01-nestjs-with-graphql/src/app.module.ts
@@ -0,0 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { BoardModule } from './apis/boards/boards.module';
+
+@Module({
+  imports: [
+    BoardModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+  ],
+})
+export class AppModule {}
