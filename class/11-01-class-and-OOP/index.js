bbf0b894f26aeb1efcb79840246685949c900f37
Tom Ko <yungsu2391@gmail.com>
2023-05-03 07:46:42 +0900

    javascript class

--- class/11-01-class-and-OOP/index.js
+++ class/11-01-class-and-OOP/index.js
@@ -1,31 +1,30 @@
 const a = new Date();
 console.log(a.getFullYear());
-console.log(a.getMonth()+1);
+console.log(a.getMonth() + 1);
 
+class Monster {
+	power = 10;
 
-class Monster{
-    power = 10
-    
-    constructor(aaa){
-        this.power = aaa
-    }
+	constructor(aaa) {
+		this.power = aaa;
+	}
 
-    attack = () => {
-        console.log('공격하자')
-        this.run();
-    }
+	attack = () => {
+		console.log("공격력은" + this.power + "야");
+		this.run();
+	};
 
-    run = () => {
-        console.log('도망가자')
-    }
+	run = () => {
+		console.log("도망가자");
+	};
 }
 
 const mymonster1 = new Monster(10);
-console.log(mymonster1.power)
+console.log(mymonster1.power);
 mymonster1.attack();
 mymonster1.run();
 
 const mymonster2 = new Monster(50);
-console.log(mymonster2.power)
+console.log(mymonster2.power);
 mymonster2.attack();
 mymonster2.run();
--- class/11-02-class-and-OOP-extends/index.js
+++ class/11-02-class-and-OOP-extends/index.js
@@ -1,38 +1,38 @@
 class Monster {
-    power = 10
-    
-    constructor(aaa){
-        this.power = aaa
-    }
+	power = 10;
+	health = 100;
 
-    attack = () => {
-        console.log('공격하자 :' + this.power)
-        this.run();
-    }
+	constructor(aaa, www) {
+		this.power = aaa;
+		this.health = www;
+	}
+
+	attack = () => {
+		console.log("공격하자 :" + this.power);
+		console.log("체력 :" + this.health);
+		this.run();
+	};
 }
 
+class SkyMonster extends Monster {
+	constructor(qqq, www) {
+		// 부모 클래스로 값 전달
+		super(qqq, www);
+	}
 
-class SkyMonster extends Monster{
-    
-    constructor(qqq){
-        // 부모 클래스로 값 전달
-        super(qqq)
-    }
-
-    run = () => {
-        console.log('날라서 도망가자')
-    }
+	run = () => {
+		console.log("날라서 도망가자");
+	};
 }
 
-class GroundMonster extends Monster{
-    
-    constructor(www){
-        super(www)    
-    }
+class GroundMonster extends Monster {
+	constructor(www) {
+		super(www);
+	}
 
-    run = () => {
-        console.log('뛰어서 도망가자')
-    }
+	run = () => {
+		console.log("뛰어서 도망가자");
+	};
 }
 
 const mymonster1 = new SkyMonster(10);
