aa9cf9766d718cf854588e993c1544a1a131a614
Tom Ko <yungsu2391@gmail.com>
2023-05-10 01:30:02 +0900

    [ ADD ] promise all

--- class/25-01-file-upload/backend/src/apis/image-uploader/image-uploader.service.ts
+++ class/25-01-file-upload/backend/src/apis/image-uploader/image-uploader.service.ts
@@ -6,39 +6,54 @@ import { Storage } from '@google-cloud/storage';
 @Injectable()
 export class ImageUploaderService {
   async create({ files }) {
-    // const waitedFiles = await Promise.all(files);
+    const waitedFiles = await Promise.all(files);
     // console.log(waitedFiles);
 
     const storage = new Storage({
       projectId: 'nestjs-file-upload',
-    })
-      .bucket('aa')
-      .file('aa');
+      keyFilename: '',
+    }).bucket('aa');
 
     //  구글 스토리지에 파일 업로드 하기
 
-    new Promise((resolve, reject) => {
-      myfile
-        .createReadStream()
-        .pipe(storage.createWriteStream())
-        .on('finish', (res) => {
-          resolve(res);
-        })
-        .on('error', (err) => {
-          reject();
-        });
-    });
+    // new Promise((resolve, reject) => {
+    //   myfile
+    //     .createReadStream()
+    //     .pipe(storage.createWriteStream())
+    //     .on('finish', (res) => {
+    //       resolve(res);
+    //     })
+    //     .on('error', (err) => {
+    //       reject();
+    //     });
+    // });
 
-    const myfile = files[0];
-    myfile
-      .createReadStream()
-      .pipe(storage.createWriteStream())
-      .on('finish', () => {
-        console.log('finish');
-      })
-      .on('error', (err) => {
-        console.log(err);
-      });
+    return await Promise.all(
+      waitedFiles.map((file) => {
+        return new Promise((resolve, reject) => {
+          file
+            .createReadStream()
+            .pipe(storage.file(file.filename).createWriteStream())
+            .on('finish', () => {
+              resolve(`aa/${file.filename}`);
+            })
+            .on('error', (err) => {
+              reject();
+            });
+        });
+      }),
+    );
+
+    // const myfile = files[0];
+    // myfile
+    //   .createReadStream()
+    //   .pipe(storage.createWriteStream())
+    //   .on('finish', () => {
+    //     console.log('finish');
+    //   })
+    //   .on('error', (err) => {
+    //     console.log(err);
+    //   });
     return ['This action adds a new imageUploader'];
   }
 
---
+++ class/26-03-promise all/index.ts
@@ -0,0 +1,86 @@
+// console.log("안녕하세요");
+const aaa = async () => {
+	const result = await new Promise((resolve, reject) => {});
+};
+
+const fetchData = async () => {
+	console.log("1");
+	console.time("=== 걸린 시간 각각 ===");
+	await new Promise((resolve, reject) => {
+		setTimeout(() => {
+			try {
+				resolve("성공시 받는 데이터");
+			} catch (error) {
+				reject("실패시 받는 데이터");
+			}
+		}, 2000);
+	}).then((res) => {
+		console.log("2");
+	});
+
+	await new Promise((resolve, reject) => {
+		setTimeout(() => {
+			try {
+				resolve("성공시 받는 데이터");
+			} catch (error) {
+				reject("실패시 받는 데이터");
+			}
+		}, 2000);
+	}).then((res) => {
+		console.log("2");
+	});
+
+	await new Promise((resolve, reject) => {
+		setTimeout(() => {
+			try {
+				resolve("성공시 받는 데이터");
+			} catch (error) {
+				reject("실패시 받는 데이터");
+			}
+		}, 2000);
+	}).then((res) => {
+		console.log("2");
+	});
+	console.timeEnd("=== 걸린 시간 각각 ===");
+	console.log("3");
+};
+
+// fetchData();
+
+const fetchData2 = async () => {
+	console.time("=== 걸린 시간 각각 ===");
+	await Promise.all([
+		new Promise((resolve, reject) => {
+			setTimeout(() => {
+				try {
+					resolve("성공시 받는 데이터");
+				} catch (error) {
+					reject("실패시 받는 데이터");
+				}
+			}, 2000);
+		}),
+		new Promise((resolve, reject) => {
+			setTimeout(() => {
+				try {
+					resolve("성공시 받는 데이터");
+				} catch (error) {
+					reject("실패시 받는 데이터");
+				}
+			}, 2000);
+		}),
+
+		new Promise((resolve, reject) => {
+			setTimeout(() => {
+				try {
+					resolve("성공시 받는 데이터");
+				} catch (error) {
+					reject("실패시 받는 데이터");
+				}
+			}, 2000);
+		}),
+	]);
+	console.timeEnd("=== 걸린 시간 각각 ===");
+	console.log("끝났다");
+};
+
+fetchData2();
