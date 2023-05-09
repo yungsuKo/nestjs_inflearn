d055ee324819704dbca5aaf110a32ad6a9baf0bf
Tom Ko <yungsu2391@gmail.com>
2023-05-09 09:07:06 +0900

    [ ADD ] Promise

--- class/25-01-file-upload/backend/src/apis/image-uploader/image-uploader.service.ts
+++ class/25-01-file-upload/backend/src/apis/image-uploader/image-uploader.service.ts
@@ -1,11 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreateImageUploaderInput } from './dto/create-image-uploader.input';
 import { UpdateImageUploaderInput } from './dto/update-image-uploader.input';
+import { Storage } from '@google-cloud/storage';
 
 @Injectable()
 export class ImageUploaderService {
-  create({ files }) {
+  async create({ files }) {
+    // const waitedFiles = await Promise.all(files);
+    // console.log(waitedFiles);
+
+    const storage = new Storage({
+      projectId: 'nestjs-file-upload',
+    })
+      .bucket('aa')
+      .file('aa');
+
     //  구글 스토리지에 파일 업로드 하기
+
+    new Promise((resolve, reject) => {
+      myfile
+        .createReadStream()
+        .pipe(storage.createWriteStream())
+        .on('finish', (res) => {
+          resolve(res);
+        })
+        .on('error', (err) => {
+          reject();
+        });
+    });
+
+    const myfile = files[0];
+    myfile
+      .createReadStream()
+      .pipe(storage.createWriteStream())
+      .on('finish', () => {
+        console.log('finish');
+      })
+      .on('error', (err) => {
+        console.log(err);
+      });
     return ['This action adds a new imageUploader'];
   }
 
